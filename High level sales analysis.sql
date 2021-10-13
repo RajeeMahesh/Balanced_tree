3b58598a27402c68e65141aad405268bfe7c3e6d
Raji <RXU200002@utdallas.edu>
2021-10-13 18:49:33 -0500

    new addition

--- High level sales analysis.sql
+++ High level sales analysis.sql
@@ -1,5 +1,3 @@
-High Level Sales Analysis
-
 1.What was the total quantity sold for all products?
 
 select product_id, product_name, sum(qty) as total_qty_purchased
@@ -7,4 +5,23 @@ from balanced_tree.product_details pd
 join balanced_tree.sales s 
 on s.prod_id = pd.product_id 
 group by product_id, product_name
+
+2.What is the total generated revenue for all products before discounts?
+select product_id, product_name, sum(s.price) as tot_rev
+from balanced_tree.product_details pd 
+join balanced_tree.sales s 
+on pd.product_id = s.prod_id 
+group by product_id, product_name
+order by tot_rev desc
+
+3. What was the total discount amount for all products?
+select product_id, product_name, sum(discount) as tot_discounts
+from balanced_tree.product_details pd 
+join balanced_tree.sales s 
+on pd.product_id = s.prod_id 
+group by product_id, product_name
+order by tot_discounts desc
+
+
+
 
--- Transaction Analysis.sql
+++ Transaction Analysis.sql
@@ -1,3 +1,19 @@
-How many unique transactions were there?
+1.How many unique transactions were there?
 select count(distinct txn_id)
 from balanced_tree.sales
+
+2.What is the average unique products purchased in each transaction?
+select round(avg(c),2) as avg_unique_prod_per_trans
+from
+  (select txn_id, count(distinct prod_id) as c
+  from balanced_tree.sales
+  group by txn_id) t1 
+
+4.What is the average discount value per transaction?
+select txn_id, round(avg(discount),2) as avg_disc
+from balanced_tree.sales 
+group by txn_id 
+order by avg_disc desc
+
+5.
+
