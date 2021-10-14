363a6704078838e7882dbaa85e5d8ba0a10606d4
Raji <RXU200002@utdallas.edu>
2021-10-14 15:55:05 -0500

    new addition

--- Transaction Analysis.sql
+++ Transaction Analysis.sql
@@ -15,5 +15,29 @@ from balanced_tree.sales
 group by txn_id 
 order by avg_disc desc
 
-5.
+5.What is the percentage split of all transactions for members vs non-members?
+select (case when member is TRUE then 'member'
+			else 'Non Member' end) as member, 
+(count(distinct txn_id)*100)/cast((select count(distinct txn_id) from balanced_tree.sales) as float) as trans_perc
+from balanced_tree.sales 
+group by member 
+
+6.What is the average revenue for member transactions and non-member transactions?
+select (case when member is TRUE then 'member'
+        	 else 'non member' end) as member, 
+      round((sum(revenue)/count(revenue)),2) as avg_revenue
+from 
+    (select member, sum(price)-discount as revenue
+    from balanced_tree.sales 
+    group by member, txn_id, discount
+    order by member) t1
+group by member
+
+
+member	avg_revenue
+false	159.12
+true	159.96
+
+
+
 
