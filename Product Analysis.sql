5b978ee5bfce428757289b45f735e34381e29e4f
RajeeMahesh <88548449+RajeeMahesh@users.noreply.github.com>
2021-10-16 13:29:02 -0500

    Update Product Analysis.sql

--- Product Analysis.sql
+++ Product Analysis.sql
@@ -7,6 +7,12 @@ group by product_name
 order by tot_rev 
 LIMIT 3 
 
+  product_name	                        tot_rev
+  Teal Button Up Shirt - Mens	        USD 12420
+  Cream Relaxed Jeans - Womens	        USD 12430
+  Navy Oversized Jeans - Womens	        USD 16562
+  
+
 2.What is the total quantity, revenue and discount for each segment?
 select *, (rev_earned - Discount_amt) as gain
 from
@@ -15,7 +21,13 @@ from
     join balanced_tree.sales s 
     on pd.product_id = s.prod_id
     group by segment_name) t1
-
+    
+  segment_name	    qty_sold    	rev_earned    	discount_amt	gain
+  Shirt	            11265	        135416	        14869	        120547
+  Jeans	            11349       	68864         	6653        	62211
+  Jacket	        11385       	121281        	12827       	108454
+  Socks	            11217       	103729        	10764       	92965
+  
 
 3.What is the top selling product for each segment?
 select segment_name, product_name, qty_pur
@@ -38,6 +50,11 @@ from
     join balanced_tree.sales s 
     on pd.product_id = s.prod_id
     group by category_name) t1
+    
+  category_name     	qty_sold    	rev_earned    	discount_amt    	gain
+    Mens              	22482       	714120        	25633           	688487
+    Womens            	22734       	575333        	19480           	555853
+
                                  
 5.What is the top selling product for each category? 
 select category_name, product_name, qty_pur
@@ -53,24 +70,76 @@ select category_name, product_name, qty_pur
 where r = 1   
 
 6.What is the percentage split of revenue by product for each segment?
-with seg as 
-    (select (rev_earned - Discount_amt) as g
-     from 
-    (select segment_name, sum(s.price*qty) as rev_earned, sum((s.price*discount)/100) as Discount_amt,                                                      
-    from balanced_tree.product_details pd 
-    join balanced_tree.sales s 
-    on pd.product_id = s.prod_id
-    group by segment_name) t2 )
- 
-
-
-select segment_name, product_name, (rev_earned - Discount_amt)/(select * from seg) 
+select t_pg.segment_name, product_name, seg_gain, ((product_gain*100)/seg_gain) as per_rev
 from
-    (select segment_name, product_name, sum(s.price*qty) as rev_earned, sum((s.price*discount)/100) as Discount_amt,                                                      
+    (select segment_name, product_name, (rev_earned - Discount_amt) as product_gain
+    from
+        (select segment_name, product_name, sum(s.price*qty) as rev_earned, sum((s.price*discount)/100) as Discount_amt                                                     
+        from balanced_tree.product_details pd 
+        join balanced_tree.sales s 
+        on pd.product_id = s.prod_id
+        group by segment_name, product_name) t1) t_pg
+join 
+ 	(select segment_name, (rev_earned - Discount_amt) as seg_gain
+     from 
+    (select segment_name, sum(s.price*qty) as rev_earned, sum((s.price*discount)/100) as Discount_amt                                                     
     from balanced_tree.product_details pd 
     join balanced_tree.sales s 
     on pd.product_id = s.prod_id
-    group by segment_name, product_name) t1
+    group by segment_name) t2) t_sg
+on t_pg.segment_name = t_sg.segment_name 
+order by segment_name, per_rev desc
+
+7.What is the percentage split of revenue by segment for each category?
+select t_sg.category_name, segment_name, c_gain as category_gain, ((s_gain*100)/c_gain) as per_rev
+from
+    (select category_name, segment_name, (rev_earned - Discount_amt) as s_gain
+    from
+        (select category_name, segment_name, sum(s.price*qty) as rev_earned, sum((s.price*discount)/100) as Discount_amt
+        from balanced_tree.product_details pd 
+        join balanced_tree.sales s 
+        on pd.product_id = s.prod_id
+        group by category_name, segment_name) t2) t_sg
+join 
+    (select category_name, (rev_earned - Discount_amt) as c_gain
+    from
+        (select category_name, sum(qty) as qty_sold, sum(s.price*qty) as rev_earned, sum((s.price*discount)/100) as Discount_amt 
+        from balanced_tree.product_details pd 
+        join balanced_tree.sales s 
+        on pd.product_id = s.prod_id
+        group by category_name) t1) t_cg
+on t_sg.category_name = t_cg.category_name
+order by category_name, per_rev
+
+category_name   	segment_name    	category_gain       	per_rev
+Mens	            Socks              	688487              	43
+Mens	            Shirt           	688487              	56
+Womens	            Jeans           	555853              	36
+Womens	            Jacket          	555853              	63
+
+8.What is the percentage split of total revenue by category?
+with tot as 
+	(
+    select (sum(price*qty) - sum((price*discount)/100)) as gain 
+    from balanced_tree.sales)
+
+
+select category_name, (rev_earned - Discount_amt) as c_gain, (((rev_earned - Discount_amt)*100)/(select * from tot)) as per_gain_in_tot_rev
+from
+        (select category_name, sum(qty) as qty_sold, sum(s.price*qty) as rev_earned, sum((s.price*discount)/100) as Discount_amt 
+        from balanced_tree.product_details pd 
+        join balanced_tree.sales s 
+        on pd.product_id = s.prod_id
+        group by category_name) t1
+        
+ category_name	        c_gain	        per_gain_in_tot_rev
+    Mens            	688487          	55
+    Womens          	555853          	44
+   
+9.What is the total transaction “penetration” for each product? (hint: penetration = number of transactions where at least 1 quantity of a product was purchased divided by total number of transactions)
+
+
+
 
 
                                          
